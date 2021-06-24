87469d6a3bdca2a8225dcd4fd08b2d4b419266d6
Maros Kukan <maros.kukan@me.com>
2021-06-24 22:41:17 +0200

    Define Login and Register forms and templates

--- README.md
+++ README.md
@@ -2,7 +2,17 @@
 
 - [Flask Blog Application](#flask-blog-application)
   - [Introduction](#introduction)
+  - [Secret Generation](#secret-generation)
 
 ## Introduction
 
 This repository contains code for building a basic blog application using Flask.
+
+## Secret Generation
+
+You can leverage the `secrets` library to generate a secret.
+
+```bash
+echo -e "import secrets\nprint(secrets.token_hex(16))" | python
+6f303dff8c5bc7c4c32870f598d2dfe9
+```
--- src/app.py
+++ src/app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '82e30eb9a16c095d5b504b44266b4d64'
 
 posts = [
     {
@@ -30,6 +32,28 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('index'))
+
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Login Unsuccessful. Please check username and password!', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
