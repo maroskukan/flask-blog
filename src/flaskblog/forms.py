89fa7e487a5954b67d9c09ede21224e3d75adced
Maros Kukan <maros.kukan@me.com>
2021-06-29 10:29:49 +0200

    Update Account page with Account Info form

--- src/flaskblog/forms.py
+++ src/flaskblog/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
 from flaskblog.models import User
+
 
 class RegistrationForm(FlaskForm):
     username         = StringField('Username',
@@ -31,3 +33,23 @@ class LoginForm(FlaskForm):
                             validators=[DataRequired()])
     remember         = BooleanField('Remember Me')
     submit           = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username         = StringField('Username',
+                            validators=[DataRequired(),Length(min=2, max=20)])
+    email            = StringField('Email',
+                            validators=[DataRequired(),Email()])
+    submit           = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')    
